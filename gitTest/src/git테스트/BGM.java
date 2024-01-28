703252cdc9612a9735b46b12e18f9de72a7373d2
USER-PC <USER-PC@116.124.48.181>
2024-01-29 01:56:14 +0900

    음악 코드 정리

--- gitTest/src/git테스트/BGM.java
+++ gitTest/src/git테스트/BGM.java
@@ -1,15 +1,20 @@
 package git테스트;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class BGM {
 	
 	public BGM() {
+		
+		
 		
 		try {
 			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("C:\\Users\\USER-PC\\Downloads\\go-for-it-80s-cheesy-vhs-infomercial-synthpop-soundtrack-187085.wav"));
@@ -26,7 +31,7 @@ public class BGM {
 			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 			
 			// 볼륨 조절
-			gainControl.setValue((-30.0f));
+			gainControl.setValue((-20.0f));
 			
 			clip.start();
 		
@@ -35,4 +40,28 @@ public class BGM {
 		}
 		
 	}
+	public void playSound(String pathName, boolean isLoop) {
+		try {
+			
+			Clip clip = AudioSystem.getClip();
+			
+			File audioFile = new File(pathName);
+			AudioInputStream coin = AudioSystem.getAudioInputStream(audioFile);
+			clip.open(coin);
+			clip.start();
+			if(isLoop) {
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+			}
+				
+		}catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}catch (UnsupportedAudioFileException e) {
+			
+		}catch (IOException e) {
+			
+		}
+			
+		
+	}
+	
 }
--- gitTest/src/git테스트/Main.java
+++ gitTest/src/git테스트/Main.java
@@ -1,12 +1,44 @@
 package git테스트;
 
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("안녕하세요");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
 		new BGM();
+		
+		while(true) {
+			System.out.println("안녕하세요 1 2 3");
+			
+			
+			
+			System.out.print("\t\t\t메뉴를 선택해"
+					+ "주세요 ! ");
+
+			int choice = sc.nextInt();
+			
+			
+			if(choice==1) {
+				System.out.println("헉");
+			}else {
+				
+				break;
+			}
+			
+		}
+		
+		
+		
+		
+		
 		
 		
 	}
